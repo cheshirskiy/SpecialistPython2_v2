6d3a9198fd5687d1d92780076eb80046dca38562
cheshirskiy <doma-kot@ya.ru>
2023-01-30 20:42:08 +0300

    Update 02_points.py

--- Module1/practice/points/02_points.py
+++ Module1/practice/points/02_points.py
@@ -3,6 +3,11 @@ class Point:
         self.x = x
         self.y = y
 
+def distance(p1: Point, p2: Point) -> float:
+    """
+    Расстояние между двумя точками
+    """
+    return ((p2.x - p1.x)**2  + (p2.y - p1.y)**2)**(0.5)
 
 # Дан список из произвольного количества точек:
 points = [Point(3, 8), Point(7, -5), Point(10, -2), Point(0, 6), Point(-12, 0)]
@@ -12,3 +17,7 @@ random_point = Point(-12, 10)
 # TODO: выведите расстояние от каждой точки(из списка) до точки random_point
 # Совет: используйте функцию distance() из предыдущего задания
 # Подсказка: смотри пример, Module1/examples/05_objects_list.py
+
+for point in points:
+    distance_to_point=distance(point, random_point)
+    print("Расстояние между точками = ",distance_to_point)
